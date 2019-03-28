8e4caffdac09ae593eb3acecb5128c7ebabfd213
Vlad Dziun <v.dziun@gmail.com>
2019-03-28 09:43:07 -0700

    add skip and attack logic

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-server.listen(1337,'127.0.0.1')
-
 var express = require("express");
-
 var path = require("path");
 app.use(express.static(path.join(__dirname, "./static")));
+server.listen(1337)
 
 var players = {};
-
 var deck = require('./static/img/deck.json');
 var whosTurn;
 var discardCard;
@@ -17,22 +14,22 @@ var discardCard;
 function Queue() {
     this.data = [];
 }
-Queue.prototype.add = function(record) {
+Queue.prototype.add = function (record) {
     this.data.unshift(record);
 }
-Queue.prototype.remove = function() {
+Queue.prototype.remove = function () {
     return this.data.pop();
 }
-Queue.prototype.size = function() {
+Queue.prototype.size = function () {
     return this.data.length;
 }
-Queue.prototype.first = function() {
+Queue.prototype.first = function () {
     return this.data[0];
 }
-Queue.prototype.delete = function(playerid) {
-    for (var i in this.data ) {
+Queue.prototype.delete = function (playerid) {
+    for (var i in this.data) {
         if (playerid.id === this.data[i].id) {
-            this.data.splice(i,1)
+            this.data.splice(i, 1)
         }
     }
 }
@@ -41,7 +38,7 @@ const turns = new Queue();
 function shuffle(array) {
     var m = array.length, t, i;
     while (m) {
-      i = Math.floor(Math.random() * m--);
+        i = Math.floor(Math.random() * m--);
         t = array[m];
         array[m] = array[i];
         array[i] = t;
@@ -49,7 +46,7 @@ function shuffle(array) {
     return array;
 }
 
-function turnManager(whosTurn){
+function turnManager(whosTurn) {
     for (i in players) {
         if (players[i].id === whosTurn.id) {
             players[i].isTurn = true;
@@ -63,10 +60,10 @@ function turnManager(whosTurn){
 shuffle(deck)
 
 function getDeck() {
-    let hand = [{id: 100, name: 'Defuse', rules: 'Negate Bomb', url: ''}];
+    let hand = [{ id: 100, name: 'Defuse', rules: 'Negate Bomb', url: '' }];
     for (let i = 0; i < 7; i++) {
         card = deck.pop()
-        if(card.id === 200) {
+        if (card.id === 200) {
             temp = card
             card = deck.pop()
             deck.push(temp)
@@ -78,6 +75,8 @@ function getDeck() {
 }
 
 io.on('connection', function (socket) { //2
+    //PLAYING MAYBE NEEDS TO BE DELETED
+    shuffle(deck)
 
     player = {
         id: socket.id,
@@ -88,71 +87,71 @@ io.on('connection', function (socket) { //2
     }
 
     players[socket.id] = player;
-    
-    socket.emit('new_user',player);
-    
+
+    socket.emit('new_user', player);
+
     io.emit('senddecktoall', deck);
 
     socket.broadcast.emit('playerjoin', players)
     socket.emit('allplayers', players)
-    
-    socket.on('updatedeck', function(data) {
+
+    socket.on('updatedeck', function (data) {
         io.emit('senddecktoall', data);
     })
-    
-    socket.on('playerdead', function(data) {
+
+    socket.on('playerdead', function (data) {
         turns.delete(players[data])
         console.log("turns")
         console.log(turns)
     })
 
-    socket.on('drewcard', function(data){
-        if(turns.size() === 1) {
+    socket.on('drewcard', function (data) {
+        if (turns.size() === 1) {
             console.log('someone has won')
             return
         }
-        // console.log("turns before", turns)
         turns.add(turns.remove())
-        // console.log("turns after", turns)
         whosTurn = turns.first()
         turnManager(whosTurn)
         socket.emit('allplayers', players)
     })
-    
+
     turns.add(player)
-    if(turns.size() === 2) {
+    if (turns.size() === 2) {
+        console.log(turns.size());
         whosTurn = turns.first();
         turnManager(whosTurn)
     }
     socket.on("discard", discard => {
         discardCard = discard;
-        // console.log("recieved discrad on server", discardCard)
         io.emit("updateddiscard", discardCard);
-        socket.emit('allplayers', players)
 
+        //Discard logic goes below
+        if (discardCard.name === "Skip") {
+            turns.add(turns.remove())
+            whosTurn = turns.first()
+            turnManager(whosTurn)
+        }
+        else if(discardCard.name === "Attack")
+        {
+            turns.add(turns.remove())
+            whosTurn = turns.first()
+            turnManager(whosTurn)
+        }
+
+        socket.emit('allplayers', players)
     })
 
-    socket.on('disconnect', function(){
-        // console.log("deleted user", players[socket.id])
+    socket.on('disconnect', function () {
         turns.delete(players[socket.id])
         delete players[socket.id]
         socket.broadcast.emit('deletedplayers', players)
-
     })
-
-    // socket.on('moved_player', function(data){
-    //     players = data
-    //     console.log(players)
-    //     io.emit('updated_positions', players)
-    // })
-
 });
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render("index");
 })
-
-//inet - 192.168.1.165
---
+++ static/js/scripts.js
@@ -0,0 +1,175 @@
+$(document).ready(function () {
+
+    var socket = io(); //1
+
+    var local_players;
+    var my_id;
+    var hand;
+    var deck;
+    var discard;
+    var isTurn = false;
+    var isAttacked = false;
+    var attackCount = 0;
+
+    socket.on('new_user', function (data) {
+        my_id = data.id
+        hand = data.hand
+        render_hand(hand)
+        $('#myid').html(my_id)
+    })
+
+    socket.on('senddecktoall', function (data) {
+        deck = data
+        $('#deckLength').html('cards left ' + deck.length);
+    })
+
+    socket.on('playerjoin', function (data) {
+        local_players = {};
+        visualizePlayers(local_players)
+        local_players = data
+        visualizePlayers(local_players)
+    })
+    socket.on('allplayers', function (data) {
+        local_players = {};
+        visualizePlayers(local_players)
+        local_players = data
+        visualizePlayers(local_players)
+    })
+    socket.on('deletedplayers', function (data) {
+        console.log("are we getting the delete call ?")
+        console.log(data)
+        local_players = {};
+        visualizePlayers(local_players)
+        local_players = data
+        visualizePlayers(local_players)
+    })
+
+    //HERE
+    socket.on('turnupdate', function (data) {
+        console.log("turnupdateon", data)
+        if (my_id === data.id) {
+            isTurn = true
+            $('#ismyturn').html('isTurn: true')
+            if(discard != undefined && discard.name === "Attack" && isTurn)
+                isAttacked = true;
+        }
+        console.log("clients istrue", isTurn)
+        console.log("Am i attacked",isAttacked)
+    })
+    socket.on("updateddiscard", data => {
+        discard = data;
+        console.log("discraded card", discard);
+        $('#discard').empty();
+        $('#discard').append('<div class="card" id="' + discard.id + '">' + discard.name + '</div>');
+
+    })
+
+    socket.on('disconnect', function (data) {
+        console.log("discon in client")
+        console.log(data)
+        local_players = data;
+        visualizePlayers(local_players)
+    })
+
+    function render_hand(hand) {
+        for (var i = 0; i < hand.length; i++) {
+            $('#hand').append('<div class="card" id="' + hand[i].id + '">' + hand[i].name + '</div>')
+        }
+    }
+
+    function visualizePlayers(players) {
+        for (var i in players) {
+            var ids = $('#' + players[i].id)
+                .map(function () { return this.id })
+                .get();
+            if (players[i].id != my_id && ids.length === 0) {
+                $('#players').append('<div class="aplayer" id="' + players[i].id + '"></div>')
+                $('#' + players[i].id).append('<p>ID:' + players[i].id + '</p><p>isTurn:' + players[i].isTurn + '</p>')
+                $('#' + players[i].id).append('<div class="cardholder"></div>')
+                for (var x in players[i].hand) {
+
+                    // console.log(players[i].hand[x].id)
+                    $('#' + players[i].id + ' .cardholder').append('<div id="' + players[i].hand[x].id + '" class="playerscard"></div>')
+                }
+            }
+        }
+    }
+
+    $('#deck').click(function () {
+        if (isTurn) {
+            cardDrawn = deck.pop()
+            //logic for exploding and defuse
+            if (cardDrawn.id === 200) {
+                console.log("drew explode")
+                for (var i in hand) {
+                    console.log(hand[i].id)
+                    if (hand[i].id === 100) {
+                        var usedDiffuse = hand.splice(i, 1)
+                        $('#hand').empty();
+                        render_hand(hand);
+                        socket.emit("discard", usedDiffuse[0]);
+                        socket.emit("drewcard", my_id)
+                        isTurn = false
+                        isAttacked = false;
+                        return
+                    }
+                }
+                console.log("you dead")
+                isTurn = false
+                isAttacked = false;
+                socket.emit('playerdead', my_id)
+            }
+            //draw and update hand
+            if(!isAttacked)
+            {
+                hand.push(cardDrawn)
+                $('#hand').append('<div class="card" id="' + hand[hand.length - 1].id + '">' + hand[hand.length - 1].name + '</div>')
+                socket.emit("drewcard", my_id)
+                socket.emit("updatedeck", deck)
+                isTurn = false;
+                $('#ismyturn').html('isTurn: false')
+            }
+            else
+            {   attackCount++;
+                hand.push(cardDrawn)
+                $('#hand').append('<div class="card" id="' + hand[hand.length - 1].id + '">' + hand[hand.length - 1].name + '</div>')
+                socket.emit("updatedeck", deck)
+                if(attackCount > 1)
+                {
+                    socket.emit("drewcard", my_id)
+                    socket.emit("updatedeck", deck)
+                    isTurn = false;
+                    isAttacked = false;
+                    socket.emit("discard", {id:666, name: "Attack1"});
+                    $('#ismyturn').html('isTurn: false')
+                }
+            }
+        } 
+        else {
+            console.log("not your turn")
+        }
+    })
+
+    $('#hand').on("click", ".card", function (e) {
+        if (isTurn) {
+            for (let i = 0; i <= hand.length; i++) {
+                if (hand[i].id === parseInt(e.target.id)) {
+                    socket.emit("discard", hand[i]);
+                    //Discard logic goes below
+                    if (hand[i].name === "Skip")
+                        isTurn = false;
+                    else if(hand[i].name === "Attack")
+                        isTurn = false;    
+                    hand.splice(i, 1);
+                    $("#hand").empty();
+                    render_hand(hand);
+                    break;
+                }
+            }
+        } else {
+            console.log("calm down not ur turn");
+        }
+    })
+
+    $('#ismyturn').html('isTurn: false')
+});
